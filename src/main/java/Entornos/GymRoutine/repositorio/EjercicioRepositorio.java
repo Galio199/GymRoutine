d5411dff1c5cd7cede8610b28b48a1c598d44a08
leider diaz <leider2191548@correo.uis.edu.co>
2024-04-10 22:35:00 -0500

    ajuste ejercicioRepository

--- src/main/java/Entornos/GymRoutine/repositorio/EjercicioRepositorio.java
+++ src/main/java/Entornos/GymRoutine/repositorio/EjercicioRepositorio.java
@@ -5,20 +5,22 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author juand
  */
+@Repository
 public interface EjercicioRepositorio extends JpaRepository<Ejercicio, Long> {
 
     //Listar los ejercicios ordenados por dificultad
-    @Query("SELECT e FROM ejercicios as e ORDER BY e.id_dificultad_ejercicio")
+    @Query("SELECT e FROM Ejercicio as e ORDER BY e.idDificultad")
     List<Ejercicio> findAllOrdenadoPorDificultad();
     
     //Filtrar ejercicios
-    @Query("SELECT e FROM ejercicios e "
-            + "WHERE (e.id_tipo_ejercicio = :idTipoEjercicio OR :idTipoEjercicio IS NULL) "
-            + "AND (e.id_dificultad_ejercicio = :idDificultadEjercicio OR :idDificultadEjercicio IS NULL)")
+    @Query("SELECT e FROM Ejercicio e "
+            + "WHERE (e.idTipoEjercicio = :idTipoEjercicio OR :idTipoEjercicio IS NULL) "
+            + "AND (e.idDificultad = :idDificultadEjercicio OR :idDificultadEjercicio IS NULL)")
     List<Ejercicio> findByTipoYDificultad(Long idTipoEjercicio, Long idDificultadEjercicio);
 }
